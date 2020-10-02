c8525675f5d5f498b79b400cda9d9e8dd1a9d7bc
ShayR <shayrashinsky@gmail.com>
2020-10-02 16:28:53 +0300

    Add ClassRoom class implementation
    Add implementation to ClassRoom Class
    
    containing setters and print function

--- src/ClassRoom.cpp
+++ src/ClassRoom.cpp
@@ -1,21 +1,30 @@
 #include "ClassRoom.h"
+#include <iostream>
 
-ClassRoom::ClassRoom(int roomNumber, int maxSeats)
-{
-	/* FIXME */
-}
+using namespace std;
+
+ClassRoom::ClassRoom(int roomNumber, int maxSeats) :
+	roomNumber(roomNumber), maxSeats(maxSeats) {}
 
 bool ClassRoom::setRoomNumber(int newRoomNumber)
 {
-	return false;
+	if (newRoomNumber < 0)
+		return false;
+
+	roomNumber = newRoomNumber;
+	return true;
 }
 bool ClassRoom::setMaxSeats(int newMaxSeats)
 {
-	return false;
+	if (newMaxSeats < 0)
+		return false;
+
+	maxSeats = newMaxSeats;
+	return true;
 }
 
-void ClassRoom::print()const
+void ClassRoom::print() const
 {
-
+	cout << "ClassRoom Number: " << roomNumber << ", contains " << maxSeats << " seats" << endl;
 }
 
