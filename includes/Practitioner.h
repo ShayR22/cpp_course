a09a92b896127a2fe9366b126ed94e6628edf895
ShayR <shayrashinsky@gmail.com>
2020-10-05 14:07:29 +0300

    Implement Practitioner class
    impelemnt Practiioner class

--- includes/Practitioner.h
+++ includes/Practitioner.h
@@ -8,17 +8,13 @@ class Practitioner : public Professor, public Student
 {
 public:
 	Practitioner(const char* name, const Date& birthDate, const char* id, eDepartmenType department, double salary,
-		int maxOfGrades = 20, int maxOfLectures = 20, int maxOfLecturesTeaching = 5);
-	Practitioner(Professor& p, Student& s); // check that p and s are the same person
-	
+		int maxOfGrades = 20, int maxOfLectures = 20, int maxOfLecturesTeaching = 5) noexcept(false);
+	Practitioner(Professor& p, Student& s) noexcept(false); // check that p and s are the same person
 
-	const Professor** assistantTo() const; //To whom he helps
+	const Professor** assistantTo(int* proffesors_size) const; //To whom he helps
 
 	/******print******/
 	virtual void print(ostream& os) const;
-
-	/******Deconstructor******/
-	~Practitioner();
 
 private:
 	Practitioner(const Practitioner& otherP) = delete;
--- src/Practitioner.cpp
+++ src/Practitioner.cpp
@@ -1,34 +1,40 @@
 #include "Practitioner.h"
 
+
+/* TODO maxOfLecturesTeaching isn't being used */
 Practitioner::Practitioner(const char* name, const Date& birthDate, const char* id, eDepartmenType department, double salary,
-	int maxOfGrades, int maxOfLectures, int maxOfLecturesTeaching) :
-	Professor(name, birthDate, id, salary, maxOfLectures), Student(name, birthDate, id, department, maxOfGrades),
-	Person(name, birthDate, id)
-{
+	int maxOfGrades, int maxOfLectures, int maxOfLecturesTeaching) noexcept(false) :
+	Person(name, birthDate, id),
+	Professor(name, birthDate, id, salary, maxOfLectures),
+	Student(name, birthDate, id, department, maxOfGrades) {}
 
-}
-
-Practitioner::Practitioner(Professor& p, Student& s) :
+Practitioner::Practitioner(Professor& p, Student& s) noexcept(false) :
 	Professor(p.getName(), p.getBirthDate(), p.getId(), p.getSalary()),
 	Student(s.getName(), s.getBirthDate(), s.getId(), s.getDepartment()),
 	Person(p.getName(), p.getBirthDate(), p.getId())
 {
-
+	if (!(s == p.getId()))
+		throw -1;
 }
- // check that p and s are the same person
-	
 
-const Professor** Practitioner::assistantTo() const
+const Professor** Practitioner::assistantTo(int *proffesors_size) const
 {
-	return nullptr;
+	int numLectures;
+	const Lecture* const* lectures = getLectures(&numLectures);
+	if (numLectures == 0)
+		return nullptr;
+
+	const Professor** assistingTo = new const Professor * [numLectures];
+	for (int i = 0; i < numLectures; i++)
+		assistingTo[i] = &lectures[i]->getLecturer();
+
+	*proffesors_size = numLectures;
+
+	return assistingTo;
 }
 
 void Practitioner::print(ostream& os) const
 {
-
-}
-
-Practitioner::~Practitioner()
-{
-
+	Professor::print(os);
+	Student::print(os);
 }
